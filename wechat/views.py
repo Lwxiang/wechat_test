93f89db3a2c8dd3ec754e8548098d9933550a8ea
lwxiang <lwxiang1994@gmail.com>
2015-08-05 09:19:14 +0000

    add checker

--- wechat/views.py
+++ wechat/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+# coding=utf8
 
-# Create your views here.
+import json
+import hashlib
+
+from django.http import HttpResponse
+
+from wechat_test.settings import token
+
+
+def checker(request):
+    if request.method == "GET":
+        signature = request.GET.get('signature')
+        timestamp = request.GET.get('timestamp')
+        nonce = request.GET.get('nonce')
+        echostr = request.GET.get('echostr')
+        que = []
+        que.append(timestamp)
+        que.append(nonce)
+        que.append(echostr)
+        que = sorted(que)
+        sign = ''
+        for s in que:
+            sign += s
+        sign = hashlib.sha1(sign)
+        if signature == sign:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse('INVALID')
+
--- wechat_test/urls.py
+++ wechat_test/urls.py
@@ -5,6 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wechat_test.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', ),
     url(r'^admin/', include(admin.site.urls)),
 )
